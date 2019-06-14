fbccc7e8c5416f4dfd382467b480e405d69de8fd
Julius-V <julius.vainora@gmail.com>
2019-06-14 13:36:50 +0200

    Style and comments

--- Problem001.py
+++ Problem001.py
@@ -1,7 +1,11 @@
-def arith_prog(d, N):
-    return d * (N // d) * ((N // d) + 1) / 2
+# The sum d + 2d + ... + kd, where k is the largest integer such that kd <= n
+def arith_prog(d, n):
+    return d * (n // d) * ((n // d) + 1) / 2
 
-def sum_mult_3n5(N):
-    return arith_prog(3, N) + arith_prog(5, N) - arith_prog(3 * 5, N);
+
+# The sum of all the distinct multiples of 3 and 5 up to 1000
+def sum_mult_3n5(n):
+    return arith_prog(3, n) + arith_prog(5, n) - arith_prog(3 * 5, n);
+
 
 print(sum_mult_3n5(1000))
