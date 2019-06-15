defb4230feca5720bdba623245918a94e7f4c54b
Julius-V <julius.vainora@gmail.com>
2019-06-15 13:09:50 +0200

    Problems 014 and 015

---
+++ Problem014.py
@@ -0,0 +1,22 @@
+
+
+# Lengths of all Collatz sequences starting from 1, ..., n
+def collatz(n):
+    lens = [0] * n
+    for i in range(1, n + 1):
+        seq = [i]
+        next = i
+        while next != 1:
+            next = next // 2 if next % 2 == 0 else 3 * next + 1
+            seq.append(next)
+            if next <= n:
+                if lens[next - 1] != 0:
+                    break
+        if next == 1:
+            lens[i - 1] = len(seq)
+        else:
+            lens[i - 1] = len(seq) + lens[next - 1] - 1
+    return lens.index(max(lens)) + 1
+
+
+print(collatz(1000000))
---
+++ Problem015.py
@@ -0,0 +1,10 @@
+from scipy.special import comb
+
+
+# The number of paths from the top left corner to the
+# bottom right corner on an n x n lattice
+def lattice_paths(n):
+    return comb(2 * n, n, exact = True)
+
+
+print(lattice_paths(20))
