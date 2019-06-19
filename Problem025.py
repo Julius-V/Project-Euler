4c30c19ffa26c6557ed6eed8a918203aa1f67a59
Julius-V <julius.vainora@gmail.com>
2019-06-20 00:16:38 +0200

    Problem 025

---
+++ Problem025.py
@@ -0,0 +1,13 @@
+
+
+# Returning the index of the first k-digit Fibonacci number
+def first_fibo_n_digits(k):
+    a, b = 0, 1
+    i = 1
+    while len(str(b)) != k:
+        a, b = b, a + b
+        i += 1
+    return i
+
+
+print(first_fibo_n_digits(1000))
