5d3fb1df13114a7d0c34e36deb9615e9293b176d
Julius-V <julius.vainora@gmail.com>
2019-06-30 00:46:16 +0200

    Faster version of Problem 039

--- Problem039.py
+++ Problem039.py
@@ -1,23 +1,18 @@
 from math import sqrt
 
 
-# Using Dickson's (1920) method to generate Pythagorean triples
-# and returning that triplet (x,y,z) that satisfies x+y+z=d
-def special_triplet(d):
-    for s in range(1, d - 1):
-        for t in range(1, d - s):
-            r = sqrt(2 * s * t)
-            if r == round(r):
-                x = r + s
-                y = r + t
-                z = r + s + t
-                if x + y + z == d:
-                    yield 1
+# Finding the numbers of times integers 0, 1, ..., ul - 1 happen to be
+# perimeters of a right triangle
+def pythagorean_perimeters(ul):
+    freqs = [0] * ul
+    for a in range(ul):
+        for b in range(ul):
+            sc = sqrt(a ** 2 + b ** 2)
+            c = int(sc)
+            p = a + b + c
+            if sc == c and p <= ul:
+                freqs[p - 1] += 1
+    return freqs.index(max(freqs)) + 1
 
 
-def most_common_perimeter():
-    freqs = [sum(special_triplet(d)) for d in range(2, 1001)]
-    return freqs.index(max(freqs)) + 2
-
-
-print(most_common_perimeter())
+print(pythagorean_perimeters(1000))
