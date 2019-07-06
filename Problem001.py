747b8dd952d862ee6facb38832317202b2e1e86c
Julius-V <julius.vainora@gmail.com>
2019-07-07 00:26:47 +0300

    Float to int and dropping semicolons

--- Problem001.py
+++ Problem001.py
@@ -1,11 +1,11 @@
 # The sum d + 2d + ... + kd, where k is the largest integer such that kd <= n
 def arith_prog(d, n):
-    return d * (n // d) * ((n // d) + 1) / 2
+    return d * (n // d) * ((n // d) + 1) // 2
 
 
 # The sum of all the distinct multiples of 3 and 5 up to 1000
 def sum_mult_3n5(n):
-    return arith_prog(3, n) + arith_prog(5, n) - arith_prog(3 * 5, n);
+    return arith_prog(3, n) + arith_prog(5, n) - arith_prog(3 * 5, n)
 
 
 print(sum_mult_3n5(1000))
