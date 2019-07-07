66d4a9bceb8a158d65a50721dd21eca0a6cd694e
Julius-V <julius.vainora@gmail.com>
2019-07-08 00:17:33 +0300

    Indenting right

--- Problem006.py
+++ Problem006.py
@@ -1,11 +1,11 @@
 # Sum of the squares of the first n positive integers
 def sum_of_squares(n):
-        return n * (n + 1) * (2 * n + 1) / 6
+    return n * (n + 1) * (2 * n + 1) // 6
 
 
 # Square of the sum of the first n positive integers
 def square_of_sum(n):
-        return (n * (n + 1) / 2) ** 2
+    return (n * (n + 1) / 2) ** 2
 
 
 print(square_of_sum(100) - sum_of_squares(100))
