8a29bb3d53cf2ec74f2ac5c9596f02b13f1fe9f3
Julius-V <julius.vainora@gmail.com>
2019-07-08 00:26:43 +0300

    Indenting

--- Problem007.py
+++ Problem007.py
@@ -3,23 +3,23 @@ from math import log, floor
 
 # Using the sieve of Sundaram to generate primes up to upper_limit
 def sieve_sundaram(upper_limit):
-        sieve_limit = floor((upper_limit - 2) / 2)
-        nums = [True] * (sieve_limit + 1)
-        nums[0] = False
-        for i in range(1, floor(sieve_limit / 2)):
-            for j in range(1, floor(sieve_limit / 2)):
-                num = i + j + 2 * i * j
-                if num <= sieve_limit:
-                    if nums[num]:
-                        nums[num] = False
-                else:
-                    break
-        return [2 * i + 1 for i in range(sieve_limit) if nums[i]]
+    sieve_limit = floor((upper_limit - 2) / 2)
+    nums = [True] * (sieve_limit + 1)
+    nums[0] = False
+    for i in range(1, floor(sieve_limit / 2)):
+        for j in range(1, floor(sieve_limit / 2)):
+            num = i + j + 2 * i * j
+            if num <= sieve_limit:
+                if nums[num]:
+                    nums[num] = False
+            else:
+                break
+    return [2 * i + 1 for i in range(sieve_limit) if nums[i]]
 
 
 # Extracting the n-th prime
 def nth_prime(n):
-        return sieve_sundaram(n * log(n) + n * log(log(n)))[n - 2]
+    return sieve_sundaram(n * log(n) + n * log(log(n)))[n - 2]
 
 
 print(nth_prime(10001))
