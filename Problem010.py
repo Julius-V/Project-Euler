d96c3eef7dc0d4f2c09ef88fcf6a7a41e42b0708
Julius-V <julius.vainora@gmail.com>
2019-07-09 01:06:43 +0300

    Indenting and a redundant import

--- Problem010.py
+++ Problem010.py
@@ -1,20 +1,20 @@
-from math import log, floor
+from math import floor
 
 
 # Using the sieve of Sundaram to generate primes up to upper_limit
 def sieve_sundaram(upper_limit):
-        sieve_limit = floor((upper_limit - 2) / 2)
-        nums = [True] * (sieve_limit + 1)
-        nums[0] = False
-        for i in range(1, floor(sieve_limit / 2)):
-            for j in range(1, floor(sieve_limit / 2)):
-                num = i + j + 2 * i * j
-                if num <= sieve_limit:
-                    if nums[num]:
-                        nums[num] = False
-                else:
-                    break
-        return [2 * i + 1 for i in range(sieve_limit) if nums[i]]
+    sieve_limit = floor((upper_limit - 2) / 2)
+    nums = [True] * (sieve_limit + 1)
+    nums[0] = False
+    for i in range(1, floor(sieve_limit / 2)):
+        for j in range(1, floor(sieve_limit / 2)):
+            num = i + j + 2 * i * j
+            if num <= sieve_limit:
+                if nums[num]:
+                    nums[num] = False
+            else:
+                break
+    return [2 * i + 1 for i in range(sieve_limit) if nums[i]]
 
 
 # Summing all the primes up to 2 million and adding 2 as the sieve of Sundaram skips it
