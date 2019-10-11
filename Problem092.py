a704273e1b0a0d709eee8d839bff5b6ac10d3bb9
Julius-V <julius.vainora@gmail.com>
2019-10-11 08:32:57 +0200

    More efficient version

--- Problem092.py
+++ Problem092.py
@@ -1,16 +1,38 @@
+from math import factorial
+
+
+# Starting from n, whether the chain ends in 1 or 89
+def square_digit_chain(n):
+    if n in [1, 89]:
+        return n
+    else:
+        return square_digit_chain(sum([int(k) ** 2 for k in str(n)]))
+
+
+# The number of starting numbers below ten million that arrive at 89
 def square_digit_chains():
-    ends = [0] * 10000000
-    for i in range(1, len(ends)):
-        num = sum([int(k) ** 2 for k in str(i)])
-        n = [i, num]
-        while num not in [1, 89]:
-            num = sum([int(k) ** 2 for k in str(n[len(n) - 1])])
-            if ends[num - 1] in [1, 89]:
-                num = ends[num - 1]
-            n.append(num)
-        for j in range(len(n)):
-            ends[n[j] - 1] = num
-    return ends.count(89)
+    s = 0
+    for ones in range(0, 7 + 1):
+        for twos in range(0, 7 + 1 - ones):
+            for threes in range(0, 7 + 1 - ones - twos):
+                for fours in range(0, 7 + 1 - ones - twos - threes):
+                    for fives in range(0, 7 + 1 - ones - twos - threes - fours):
+                        for sixes in range(0, 7 + 1 - ones - twos - threes - fours - fives):
+                            for sevens in range(0, 7 + 1 - ones - twos - threes - fours - fives - sixes):
+                                for eights in range(0, 7 + 1 - ones - twos - threes - fours - fives - sixes - sevens):
+                                    for nines in range(0, 7 + 1 - ones - twos - threes - fours - fives - sixes - sevens - eights):
+                                        for zeroes in range(0, 7 + 1 - ones - twos - threes - fours - fives - sixes - sevens - eights - nines):
+                                            if ones + twos + threes + fours + fives + \
+                                                    sixes + sevens + eights + nines + zeroes == 7 and zeroes < 7:
+                                                n = ones + twos * 4 + threes * 9 + fours * 16 + fives * 25 + \
+                                                    sixes * 36 + sevens * 49 + eights * 64 + nines * 81
+                                                if square_digit_chain(n) == 89:
+                                                    s += factorial(7) // (factorial(ones) * factorial(twos) *
+                                                                          factorial(threes) * factorial(fours) *
+                                                                          factorial(fives) * factorial(sixes) *
+                                                                          factorial(sevens) * factorial(eights) *
+                                                                          factorial(nines) * factorial(zeroes))
+    return s
 
 
 print(square_digit_chains())
