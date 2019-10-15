5783320393e6f05d345db0fbc707a493297d1928
Julius-V <julius.vainora@gmail.com>
2019-10-15 10:00:32 +0200

    A better solution

--- Problem206.py
+++ Problem206.py
@@ -1,19 +1,33 @@
-from math import ceil, floor, sqrt
+from math import floor, sqrt
 
 
-# Check if n is of the form 1_2_3_4_5_6_7_8_9_0
-def magic_form(n):
-    n_str = str(n)
-    return [n_str[i] for i in range(0, 19, 2)] == list(map(str, list(range(1, 10)) + [0]))
-
+# n^2 = 0 mod 10 if and only if n ends with a zero
+#
+# In that case 1_2_3_4_5_6_7_8_9_0 = 1_2_3_4_5_6_7_8_900 and (n / 10)^2 = 1_2_3_4_5_6_7_8_9
+# It follows that (n / 10) = 3 or 7 mod 10
 
 # Finding the unique integer whose square is of the form 1_2_3_4_5_6_7_8_9_0
 def unique_root():
-    k = floor(sqrt(1020304050607080900))
-    k = 1389019170 - 10
-    while not magic_form(k ** 2):
-        k += 1
-    return k
+    k = floor(sqrt(10203040506070809)) // 10 * 10 + 3
+    aux = [str(i) for i in range(1, 10)]
+    while True:
+        idx = True
+        n = str(k ** 2)
+        for i in range(0, 17, 2):
+            idx = n[i] == aux[i // 2]
+            if not idx:
+                break
+        if idx:
+            return k
+        k += 4
+        n = str(k ** 2)
+        for i in range(0, 17, 2):
+            idx = n[i] == aux[i // 2]
+            if not idx:
+                break
+        if idx:
+            return k
+        k += 6
 
 
 print(unique_root())
